0a5aa767e50d83006bf2bc26fa5af5ea8a0becc5
delaworla <worlanuel@hotmail.com>
2022-08-02 22:28:30 +0000

    Test for GET customers

--- api/tests/test_customer_api.py
+++ api/tests/test_customer_api.py
@@ -1,13 +1,13 @@
 from django.urls import reverse, include, resolve
 from django.test import SimpleTestCase
-
+from api.views import CustomerView
 
 
 class ApiUrlsTests(SimpleTestCase):
 
     def test_get_customers_is_resolved(self):
         url = reverse('customer')
-        print(url)
+        self.assertEquals(resolve(url).func.view_class, CustomerView)
         
 
 
