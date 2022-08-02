610a112d3d1d2aaf198ac54b6ce2c0f2e24ae660
delaworla <worlanuel@hotmail.com>
2022-08-02 22:49:36 +0000

    Authentication test for GET Customers

--- api/tests/test_customer_api.py
+++ api/tests/test_customer_api.py
@@ -1,6 +1,13 @@
 from django.urls import reverse, include, resolve
 from django.test import SimpleTestCase
 from api.views import CustomerView
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+
+
 
 
 class ApiUrlsTests(SimpleTestCase):
@@ -8,6 +15,19 @@ class ApiUrlsTests(SimpleTestCase):
     def test_get_customers_is_resolved(self):
         url = reverse('customer')
         self.assertEquals(resolve(url).func.view_class, CustomerView)
-        
+    
+    
+class CustomerAPIViewTests(APITestCase):
+    customers_url = reverse("customer")
 
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='admin', password='django')
+        self.token = Token.objects.create(user=self.user)
+        #self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_get_customers_authenticated(self):
+        response = self.client.get(self.customers_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
