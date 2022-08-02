4df5b7d2e715996ca55b118d16828cdaf145d80b
delaworla <worlanuel@hotmail.com>
2022-08-02 20:05:49 +0000

    Forming views

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'business'
+    'business',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
