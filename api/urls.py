916ffd2052d0efe9353ac4317fdd0d4f8002d6af
delaworla <worlanuel@hotmail.com>
2022-08-02 22:14:32 +0000

    Initializing Tests

--- api/urls.py
+++ api/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('customers/', api_views.CustomerView.as_view(), name="customer"),
     path('customers/<int:pk>/', api_views.CustomerDetailView.as_view(), name="customer-detail"),
     path('api-token-auth/', views.obtain_auth_token),
-    
+
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+# token expires after 5 minutes
